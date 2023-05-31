91198e971a9653408873dde77c96b9b31c1af264
Qorri Dwi Istajib <qorri24j@gmail.com>
2023-05-31 07:51:40 +0700

    template convert file first project

--- spring-latihan/src/main/java/com/qodev/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.qodev.repository;
-
-import com.qodev.domain.MasterUser;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<MasterUser, Integer> {
-}
